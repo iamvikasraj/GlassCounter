3e7c3e4e09b4e1fb95e27f4a3221a73b1c0001f7
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-07-28 21:43:59 +0530

    Initial Commit

---
+++ GlassCounter/GlassCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassCounterApp.swift
+//  GlassCounter
+//
+//  Created by Vikas Raj Yadav on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
